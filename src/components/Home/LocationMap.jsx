541c7b9612d2e49622eef5defbd0a8780470e2c6
orion <munna.csehstu@gmail.com>
2025-01-24 00:55:51 +0600

    added map

---
+++ src/components/Home/LocationMap.jsx
@@ -0,0 +1,56 @@
+import { MapContainer, TileLayer, Marker, Tooltip, Circle } from "react-leaflet";
+import L from "leaflet"; // Leaflet for custom icons
+import "leaflet/dist/leaflet.css";
+
+// Custom red marker icon
+const redIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconSize: [25, 41], // Size of the icon
+  iconAnchor: [12, 41], // Anchor point of the icon
+  popupAnchor: [1, -34], // Popup anchor
+  shadowSize: [41, 41], // Size of the shadow
+});
+
+const LocationMap = () => {
+  const position = [23.884130433910936, 90.38799667444884]; // Uttara Sector 10 coordinates
+
+  return (
+    <div>
+      <div style={{ height: "500px", width: "100%" }}>
+        <MapContainer
+          center={position}
+          zoom={13} // Zoom level
+          style={{ height: "100%", width: "100%" }}
+        >
+          {/* Add TileLayer for the map tiles */}
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+
+          {/* Marker with custom red icon */}
+          <Marker position={position} icon={redIcon}>
+            {/* Tooltip that is always visible */}
+            <Tooltip permanent>
+              <span>Uttara Sector 10, Dhaka</span>
+            </Tooltip>
+          </Marker>
+
+          {/* Red dotted circle around the marker */}
+          <Circle
+            center={position}
+            radius={500} // Radius in meters
+            pathOptions={{
+              color: "red", // Border color
+              fillColor: "transparent", // No fill inside the circle
+              dashArray: "5, 10", // Dotted border
+            }}
+          />
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
+
+export default LocationMap;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import LoadingSpinner from "../../components/Shared/LoadingSpinner";
 import useApartments from "../../hooks/useApartments";
 import Title from "../../components/Home/Title";
 import { Link } from "react-router-dom";
+import LocationMap from "../../components/Home/LocationMap";
 
 const Home = () => {
     const [minimum, setMinimum] = useState('');
@@ -26,10 +27,11 @@ const Home = () => {
             {/* apartments section */}
             <section className="my-20">
                 <Container>
-                    {/* apartments card */}
+                    {/* section title */}
                     <div>
                         <Title title='Get Your Apartment' des='Explore a diverse range of apartments, from budget-friendly options to premium luxury residences. Begin your journey with us today and take the first step toward your new home.'></Title>
                     </div>
+                    {/* apartments card */}
                     <div>
                         {
                             isLoading ? <LoadingSpinner></LoadingSpinner> :
@@ -48,6 +50,55 @@ const Home = () => {
                     </div>
                     <div className="flex justify-center mt-10">
                         <Link to='/apartments' className="btn  text-white bg-green-500 hover:bg-green-600">See More</Link>
+                    </div>
+                </Container>
+            </section>
+
+            {/* about the building section */}
+            <section className="my-20">
+                <Container>
+                    {/* section title */}
+                    <div>
+                        <Title title='About the Building' des='Explore a diverse range of apartments, from budget-friendly options to premium luxury residences. Begin your journey with us today and take the first step toward your new home.'></Title>
+                    </div>
+                    {/* building details content */}
+                    <div>
+                        <div className='flex justify-between  min-h-[500px]'>
+                            {/* left */}
+                            <div className='flex gap-3 w-[45%]'>
+                                <div>
+                                    <img className='w-[396px] h-full object-cover rounded-3xl' src="https://i.ibb.co.com/PmL2FVy/1.jpg" alt="" />
+                                </div>
+
+                                <div className='flex flex-col gap-3'>
+                                    <img className='w-[167px]  h-[50%] object-cover rounded-3xl' src="https://i.ibb.co.com/55Bxqwb/road-green.jpg" alt="" />
+                                    <img className='w-[167px] h-[50%] object-cover rounded-3xl' src="https://i.ibb.co.com/52tYYNW/4.jpg" alt="" />
+                                </div>
+                            </div>
+                            {/* right */}
+                            <div className='w-1/2'>
+                                <h1 className='font-bold text-6xl leading-[75px]'>Bringing <span className='text-green-600'>Dreams to Reality</span> with Love & Care!</h1>
+                                <p className='mt-5 text-xl'>Explore a diverse range of apartments, from budget-friendly options to premium luxury residences.Get in-depth details on property features, pricing, and neighborhood highlights.Experience properties as if you're there through detailed photos and virtual walkthroughs.</p>
+
+
+                                <Link to='/apartments' className='mt-5 btn text-white bg-green-500 hover:bg-green-600'>Explore Your Dreams </Link>
+                            </div>
+
+                        </div>
+                    </div>
+                </Container>
+            </section>
+
+            {/* Location */}
+            <section className="my-20">
+                <Container>
+                    {/* section title */}
+                    <div>
+                        <Title title='Building Location' des='Explore a diverse range of apartments, from budget-friendly options to premium luxury residences. Begin your journey with us today and take the first step toward your new home.'></Title>
+                    </div>
+                    {/* display map */}
+                    <div>
+                        <LocationMap></LocationMap>
                     </div>
                 </Container>
             </section>
