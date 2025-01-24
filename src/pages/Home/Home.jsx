8163725cecaf33aac28555287561723d892d8058
orion <munna.csehstu@gmail.com>
2025-01-24 14:28:48 +0600

    completed about the building section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,12 @@ import useApartments from "../../hooks/useApartments";
 import Title from "../../components/Home/Title";
 import { Link } from "react-router-dom";
 import LocationMap from "../../components/Home/LocationMap";
+import { GiIsland } from "react-icons/gi";
+import { MdApartment, MdOutlineSportsGymnastics, MdRoofing } from "react-icons/md";
+import { FaBuilding, FaRegBuilding } from "react-icons/fa";
+import { LuCircleParking } from "react-icons/lu";
+import { TbPlayBasketball } from "react-icons/tb";
+import { FaHouseTsunami } from "react-icons/fa6";
 
 const Home = () => {
     const [minimum, setMinimum] = useState('');
@@ -62,26 +68,88 @@ const Home = () => {
                         <Title title='About the Building' des='Explore a diverse range of apartments, from budget-friendly options to premium luxury residences. Begin your journey with us today and take the first step toward your new home.'></Title>
                     </div>
                     {/* building details content */}
-                    <div>
-                        <div className='flex justify-between  min-h-[500px]'>
+                    <div >
+                        <div className='flex justify-between min-h-[500px]'>
                             {/* left */}
-                            <div className='flex gap-3 w-[45%]'>
+                            <div className='flex gap-3 w-[60%]'>
                                 <div>
-                                    <img className='w-[396px] h-full object-cover rounded-3xl' src="https://i.ibb.co.com/PmL2FVy/1.jpg" alt="" />
+                                    <img className='w-[520px] h-full object-cover rounded-3xl' src="https://i.ibb.co.com/PmL2FVy/1.jpg" alt="" />
                                 </div>
 
                                 <div className='flex flex-col gap-3'>
-                                    <img className='w-[167px]  h-[50%] object-cover rounded-3xl' src="https://i.ibb.co.com/55Bxqwb/road-green.jpg" alt="" />
-                                    <img className='w-[167px] h-[50%] object-cover rounded-3xl' src="https://i.ibb.co.com/52tYYNW/4.jpg" alt="" />
+                                    <img className='w-[200px]  h-[50%] object-cover rounded-3xl' src="https://i.ibb.co.com/55Bxqwb/road-green.jpg" alt="" />
+                                    <img className='w-[200px] h-[50%] object-cover rounded-3xl' src="https://i.ibb.co.com/52tYYNW/4.jpg" alt="" />
                                 </div>
                             </div>
                             {/* right */}
-                            <div className='w-1/2'>
-                                <h1 className='font-bold text-6xl leading-[75px]'>Bringing <span className='text-green-600'>Dreams to Reality</span> with Love & Care!</h1>
-                                <p className='mt-5 text-xl'>Explore a diverse range of apartments, from budget-friendly options to premium luxury residences.Get in-depth details on property features, pricing, and neighborhood highlights.Experience properties as if you're there through detailed photos and virtual walkthroughs.</p>
+                            <div className=''>
+                                <div className="flex flex-col gap-3">
+                                    {/* building at a glance */}
+                                    <div className="space-y-1">
+                                        <p className="text-3xl font-bold mb-3">Building <span className="text-green-600">At a Glance:</span></p>
+                                        {/* total build up area */}
+                                        <div className="flex gap-2 items-center">
+                                            <GiIsland size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Total Build-Up Area :</span> Over 50,000 Sq. Ft.</p>
+                                        </div>
+                                        {/* Common Area size */}
+                                        <div className="flex gap-2 items-center">
+                                            <MdApartment size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Floors :</span> 10-Story Residential Tower</p>
+                                        </div>
+                                        {/* Total Apartments */}
+                                        <div className="flex gap-2 items-center">
+                                            <MdApartment size={25} />
+                                            <p className="text-xl"><span className="font-semibold"> Apartments :</span> 20 Premium Apartments.</p>
+                                        </div>
+                                        {/* parking Area size */}
+                                        <div className="flex gap-2 items-center">
+                                            <LuCircleParking size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Parking :</span> Parking for Each Unit, Plus Visitor Parking.</p>
+                                        </div>
+
+                                    </div>
+
+                                    {/* apartments at a glance */}
+                                    <div className="space-y-1">
+                                        <p className="text-3xl font-bold mb-3">Apartments <span className="text-green-600">At a Glance:</span></p>
+                                        {/* apartment size */}
+                                        <div className="flex gap-2 items-center">
+                                            <MdApartment size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Apartments Size :</span> 1,200 to 2,500 Sq. Ft.</p>
+                                        </div>
+                                        {/* apartment size */}
+                                        <div className="flex gap-2 items-center">
+                                            <FaHouseTsunami size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Rooms :</span> 2-4 Rooms in Each Apartment.</p>
+                                        </div>
+
+                                    </div>
 
 
-                                <Link to='/apartments' className='mt-5 btn text-white bg-green-500 hover:bg-green-600'>Explore Your Dreams </Link>
+                                    {/* community features */}
+                                    <div className="space-y-1">
+                                        <p className="text-3xl font-bold mb-3">Features <span className="text-green-600">At a Glance:</span></p>
+                                        {/* Gym Area size */}
+                                        <div className="flex gap-2 items-center">
+                                            <MdOutlineSportsGymnastics size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Gym :</span> A Modern well Equipped Gymnasium.</p>
+                                        </div>
+                                        {/* Gym Area size */}
+                                        <div className="flex gap-2 items-center">
+                                            <MdRoofing size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Rooftop :</span> Garden and Swimming Pool.</p>
+                                        </div>
+                                        {/* children Area size */}
+                                        <div className="flex gap-2 items-center">
+                                            <TbPlayBasketball size={25} />
+                                            <p className="text-xl"><span className="font-semibold">Kids Zone :</span> Dedicated for Children's Playing.</p>
+                                        </div>
+                                    </div>
+
+                                    {/* see more details button */}
+                                    <Link to='/apartments' className='mt-3 w-[130px] btn text-white bg-green-500 hover:bg-green-600'>More details </Link>
+                                </div>
                             </div>
 
                         </div>
@@ -99,8 +167,8 @@ const Home = () => {
                     {/* display map */}
                     <div>
                         <div className='flex justify-between  min-h-[500px]'>
-                             {/* left */}
-                             <div className='w-1/2'>
+                            {/* left */}
+                            <div className='w-1/2'>
                                 <h1 className='font-bold text-6xl leading-[75px]'>Bringing <span className='text-green-600'>Dreams to Reality</span> with Love & Care!</h1>
                                 <p className='mt-5 text-xl'>Explore a diverse range of apartments, from budget-friendly options to premium luxury residences.Get in-depth details on property features, pricing, and neighborhood highlights.Experience properties as if you're there through detailed photos and virtual walkthroughs.</p>
 
@@ -109,13 +177,13 @@ const Home = () => {
                             </div>
                             {/* right */}
                             <div className='flex gap-3 w-[45%]'>
-                                
 
-                           
-                                   <LocationMap></LocationMap>
-                             
+
+
+                                <LocationMap></LocationMap>
+
                             </div>
-                          
+
                         </div>
                     </div>
                 </Container>
