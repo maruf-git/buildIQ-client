267462d6882d4d7415a6f3718c95d33f9d94d5d9
orion <munna.csehstu@gmail.com>
2025-01-25 12:01:09 +0600

    validate register form and improved design of register and login form

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -78,7 +78,7 @@ const Login = () => {
         <div className='w-full px-6 py-8 md:px-8 '>
 
 
-          <p className='mt-3 text-xl text-center text-gray-600 '>
+          <p className='mt-3 text-xl text-center text-green-600 '>
             Welcome back!
           </p>
 
@@ -133,7 +133,7 @@ const Login = () => {
                 id='LoggingEmailAddress'
                 autoComplete='email'
                 name='email'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-green-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-green-300'
                 type='email'
               />
             </div>
@@ -152,14 +152,14 @@ const Login = () => {
                 id='loggingPassword'
                 autoComplete='current-password'
                 name='password'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-green-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-green-300'
                 type='password'
               />
             </div>
             <div className='mt-6'>
               <button
                 type='submit'
-                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
+                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-300 focus:ring-opacity-50'
               >
                 Sign In
               </button>
--- src/pages/authentication/Register.jsx
+++ src/pages/authentication/Register.jsx
@@ -1,16 +1,19 @@
 import { Link, useNavigate } from 'react-router-dom'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from '../../providers/AuthProvider'
 import toast from 'react-hot-toast'
 import axios from 'axios'
 import useAxiosSecure from '../../hooks/useAxiosSecure'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 
 const Register = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { signInWithGoogle, createUser, updateUserProfile, setUser, setRole } =
     useContext(AuthContext);
   const axiosSecure = useAxiosSecure();
+  const [validationMessage, setValidationMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSignUp = async (e) => {
     e.preventDefault()
@@ -19,6 +22,40 @@ const Register = () => {
     const name = form.name.value
     const photo = form.photo.value
     const pass = form.password.value
+
+    if (pass.length < 6) {
+      setValidationMessage("Password must be at least 6 characters");
+      return;
+    }
+    if (name.trim() === "") {
+      setValidationMessage("Name can not be empty");
+      return;
+    }
+    if (photo.trim() === "") {
+      setValidationMessage("Photo URL can not be empty");
+      return;
+    }
+    const uppercaseRegex = /[A-Z]/;
+    const numberRegex = /\d/;
+    const specialRegex = /[^a-zA-Z0-9\s]/;
+    const lowercaseRegex = /[a-z]/;
+    // validation
+    if (!lowercaseRegex.test(pass)) {
+      setValidationMessage("Password must have at least one lowercase letter");
+      return;
+    }
+    if (!uppercaseRegex.test(pass)) {
+      setValidationMessage("Password must have at least one uppercase letter");
+      return;
+    }
+    if (!specialRegex.test(pass)) {
+      setValidationMessage("Password must have at least one special character");
+      return;
+    }
+    if (!numberRegex.test(pass)) {
+      setValidationMessage("Password must have at least one number");
+      return;
+    }
 
     try {
       //2. User Registration
@@ -41,8 +78,12 @@ const Register = () => {
       console.log(err)
       toast.error(err?.message)
     }
-
   }
+  // handle password show eye button
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  }
+  console.log('validate message:', validationMessage);
 
   // Google Signin
   const handleGoogleSignIn = async () => {
@@ -82,7 +123,7 @@ const Register = () => {
     <div className='flex justify-center items-center min-h-[calc(100vh-306px)] my-12'>
       <div className='flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg '>
         <div className='w-full px-6 py-8 md:px-8'>
-          <p className='mt-3 text-xl text-center text-gray-600 '>
+          <p className='mt-3 text-xl text-center text-green-600 '>
             Get Your Free Account Now.
           </p>
           {/* google sign in option */}
@@ -124,8 +165,10 @@ const Register = () => {
             </div>
             <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
           </div>
+
           {/* register with email and password */}
           <form onSubmit={handleSignUp}>
+            {/* name */}
             <div className='mt-4'>
               <label
                 className='block mb-2 text-sm font-medium text-gray-600 '
@@ -138,10 +181,11 @@ const Register = () => {
                 id='name'
                 autoComplete='name'
                 name='name'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-green-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-green-300'
                 type='text'
               />
             </div>
+            {/* photo url */}
             <div className='mt-4'>
               <label
                 className='block mb-2 text-sm font-medium text-gray-600 '
@@ -154,10 +198,11 @@ const Register = () => {
                 id='photo'
                 autoComplete='photo'
                 name='photo'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
-                type='text'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-green-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-green-300'
+                type='url'
               />
             </div>
+            {/* email */}
             <div className='mt-4'>
               <label
                 className='block mb-2 text-sm font-medium text-gray-600 '
@@ -170,12 +215,12 @@ const Register = () => {
                 id='LoggingEmailAddress'
                 autoComplete='email'
                 name='email'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-green-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-green-300'
                 type='email'
               />
             </div>
-
-            <div className='mt-4'>
+            {/* password */}
+            <div className='mt-4 relative'>
               <div className='flex justify-between'>
                 <label
                   className='block mb-2 text-sm font-medium text-gray-600 '
@@ -186,18 +231,32 @@ const Register = () => {
               </div>
 
               <input
+                type={`${showPassword ? "text" : "password"}`}
                 required
                 id='loggingPassword'
-                autoComplete='current-password'
+                autoComplete="current-password"
                 name='password'
-                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300'
-                type='password'
+                className='block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-green-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-green-300'
               />
+              <button
+                type="button"
+                onClick={handleShowPassword}
+                className="absolute inset-y-0 right-3 top-[25px] text-gray-500"
+                aria-label="Toggle Password Visibility"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+            {/* validation message */}
+            <div className='mt-4'>
+              {
+                validationMessage && <p className='text-red-600'>{validationMessage} hello</p>
+              }
             </div>
             <div className='mt-6'>
               <button
                 type='submit'
-                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50'
+                className='w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-500 rounded-lg hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-300 focus:ring-opacity-50'
               >
                 Sign Up
               </button>
